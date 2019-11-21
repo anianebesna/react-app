688abf3e5e625a20d09f295bde4c74e99f8d7368
Ania <ania-nebesna@ukr.net>
2019-11-21 23:36:59 +0200

    Updates

---
+++ precache-manifest.35b42bdee54c57e4b6e52524747b92d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88511b8ea857b9fa712e9fb3f677d7f",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "b98dd78eee48135db506",
+    "url": "/react-app/static/css/main.16257dad.chunk.css"
+  },
+  {
+    "revision": "3d062743c5ea104dc9cf",
+    "url": "/react-app/static/js/2.8478d7ce.chunk.js"
+  },
+  {
+    "revision": "b98dd78eee48135db506",
+    "url": "/react-app/static/js/main.2c106d82.chunk.js"
+  },
+  {
+    "revision": "e50508ab6773106fc348",
+    "url": "/react-app/static/js/runtime-main.689c98ea.js"
+  },
+  {
+    "revision": "df4abd46d1fd6698d1812700f5ff35fe",
+    "url": "/react-app/static/media/bg-main.df4abd46.png"
+  },
+  {
+    "revision": "5c9030c64fe5d346b6b75d6d52a69d6d",
+    "url": "/react-app/static/media/img-vector.5c9030c6.png"
+  }
+]);
